45f3d17dbb3693a24af1739305f08a0d219d4a4d
ypsilonx <cibulec.tom@gmail.com>
2023-02-02 19:15:10 +0100

    přidání kotroly napsání vstupu

--- prvni app/Kalkulacka/Program.cs
+++ prvni app/Kalkulacka/Program.cs
@@ -14,28 +14,48 @@ namespace Kalkulacka
             Console.WriteLine("Ahoj vítám tě v jednoduché kalkulačce.");
             Console.WriteLine("Zadej první číslo:");
             string prvni_cislo = Console.ReadLine();
-            float first = float.Parse(prvni_cislo);
+            double first;
+            bool napsalCislo1 = double.TryParse(prvni_cislo, out first);
+            if(napsalCislo1)
+            {
+                Console.WriteLine("Je to cajk.");
+            }
+            else
+            {
+                Console.WriteLine("Není to číslo kámo.");
+                return;
+            }
             Console.WriteLine("Zadej druhé číslo:");
             string druhe_cislo = Console.ReadLine();
-            float second = float.Parse(druhe_cislo);
+            double second;
+            bool napsalCislo2 = double.TryParse(druhe_cislo, out second);
+            if(napsalCislo2)
+            {
+                Console.WriteLine("Je to cajk.");
+            }
+            else
+            {
+                Console.WriteLine("Není to číslo kámo.");
+                return;
+            }
             Console.WriteLine("Vyber operator: (nabídka: +;-;*;/)");
             string operator_kalk = Console.ReadLine();
             if (operator_kalk == "+")
             {
                 Console.WriteLine("Vybral si operátor " + operator_kalk);
-                float vysledek_kalk = first + second;
+                double vysledek_kalk = first + second;
                 Console.WriteLine("Výsledek je " + vysledek_kalk);
             }
             else if (operator_kalk == "-")
             {
                 Console.WriteLine("Vybral si operátor " + operator_kalk);
-                float vysledek_kalk = first - second;
+                double vysledek_kalk = first - second;
                 Console.WriteLine("Výsledek je " + vysledek_kalk);
             }
             else if (operator_kalk == "*")
             {
                 Console.WriteLine("Vybral si operátor " + operator_kalk);
-                float vysledek_kalk = first * second;
+                double vysledek_kalk = first * second;
                 Console.WriteLine("Výsledek je " + vysledek_kalk);
             }
             else if (operator_kalk == "/")
@@ -46,7 +66,7 @@ namespace Kalkulacka
                     Console.WriteLine("Nelze dělit nulou!");
                     Environment.Exit(0);
                 }
-                float vysledek_kalk = first / second;
+                double vysledek_kalk = first / second;
                 Console.WriteLine("Výsledek je " + vysledek_kalk);
             }
             else
